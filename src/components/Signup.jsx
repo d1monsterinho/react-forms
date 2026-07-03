f0fd303d8187d0d6f9a929bd2377d258f652bb5c
Dmytro Yevich <dmytro.yevich@cengage.com>
2026-07-03 19:25:37 +0300

    Implement signup passwords  validation on keystroke and focus lost

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,4 +1,37 @@
+import {useState} from "react";
+
 export default function Signup() {
+    const [passwords, setPasswords] = useState({
+        password: '',
+        'password-confirm': '',
+    });
+
+    const [isEditingPasswords, setIsEditingPasswords] = useState({
+        password: true,
+        'password-confirm': true,
+    });
+
+    let passwordsDoNotMatch = (!isEditingPasswords.password && !isEditingPasswords['password-confirm'])
+        && passwords.password !== passwords["password-confirm"];
+
+    function handlePasswordBlur(identifier) {
+        setIsEditingPasswords(prev => ({
+            ...prev,
+            [identifier]: false,
+        }));
+    }
+
+    function handlePasswordChange(identifier, event) {
+        setPasswords(prev => ({
+            ...prev,
+            [identifier]: event.target.value,
+        }));
+
+        setIsEditingPasswords(prev => ({
+            ...prev,
+            [identifier]: true,
+        }));
+    }
 
     function handleSubmit(event) {
         event.preventDefault();
@@ -24,7 +57,17 @@ export default function Signup() {
             <div className="control-row">
                 <div className="control">
                     <label htmlFor="password">Password</label>
-                    <input id="password" type="password" name="password" />
+                    <input
+                        id="password"
+                        type="password"
+                        name="password"
+                        onBlur={() => handlePasswordBlur('password')}
+                        onChange={(event) => handlePasswordChange('password', event)}
+                    />
+
+                    <div className="control-error">
+                        {passwordsDoNotMatch && <p>Passwords should match</p>}
+                    </div>
                 </div>
 
                 <div className="control">
@@ -33,7 +76,15 @@ export default function Signup() {
                         id="confirm-password"
                         type="password"
                         name="confirm-password"
+                        onBlur={() => handlePasswordBlur('password-confirm')}
+                        onChange={(event) => handlePasswordChange('password-confirm', event)}
                     />
+
+                    {passwordsDoNotMatch && (
+                        <div className="control-error">
+                            <p>Passwords should match</p>
+                        </div>)}
+
                 </div>
             </div>
 
