79de5e4be6f456d90fd93892df0bc9cf1a3b5e0f
Dmytro Yevich <dmytro.yevich@cengage.com>
2026-07-03 14:01:34 +0300

    Do reformat

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import Header from './components/Header.jsx';
 import Login from './components/Login.jsx';
 
 function App() {
-  return (
-    <>
-      <Header />
-      <main>
-        <Login />
-      </main>
-    </>
-  );
+    return (
+        <>
+            <Header/>
+            <main>
+                <Login/>
+            </main>
+        </>
+    );
 }
 
 export default App;
