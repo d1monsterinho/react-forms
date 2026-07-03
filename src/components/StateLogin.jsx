aac490fb8f7e4634ef38bd069dcccb22c82ca055
Dmytro Yevich <dmytro.yevich@cengage.com>
2026-07-03 14:35:04 +0300

    Implement state-based form handling

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -1,7 +1,21 @@
+import {useState} from "react";
+
 export default function Login() {
+    const [enteredForm, setEnteredForm] = useState({
+        email: '',
+        password: '',
+    });
+
+    function onFormChange(field, event) {
+        setEnteredForm(prev => ({
+            ...prev,
+            [field]: event.target.value
+        }));
+    }
+
     function onLogin(event) {
         event.preventDefault();
-        console.log('On Login');
+        console.log(enteredForm.email, enteredForm.password);
     }
 
     return (
@@ -15,6 +29,8 @@ export default function Login() {
                         id="email"
                         type="email"
                         name="email"
+                        onChange={(event) => onFormChange('email', event)}
+                        value={enteredForm.email}
                     />
                 </div>
 
@@ -24,6 +40,8 @@ export default function Login() {
                         id="password"
                         type="password"
                         name="password"
+                        onChange={(event) => onFormChange('password', event)}
+                        value={enteredForm.password}
                     />
                 </div>
             </div>
